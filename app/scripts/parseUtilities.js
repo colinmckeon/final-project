42e8c4848982fac0b0d4577b89884fb0fdabaed9
Colin McKeon <14mckeon@gmail.com>
2016-11-15 12:38:48 -0500

    gettin info from API

---
+++ app/scripts/parseUtilities.js
@@ -0,0 +1,19 @@
+
+
+function setupParse(appId, apiKey, sessionId){
+  $.ajaxSetup({
+    beforeSend: function(xhr){
+      xhr.setRequestHeader("X-Parse-Application-Id", appId);
+      xhr.setRequestHeader("X-Parse-REST-API-Key", apiKey);
+
+      if(sessionId){
+        xhr.setRequestHeader("X-Parse-Session-Token", sessionId);
+
+      }
+    }
+  });
+}
+
+module.exports = {
+  setupParse: setupParse
+}
--- app/scripts/router.js
+++ app/scripts/router.js
@@ -2,17 +2,43 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var Backbone = require('backbone');
 
-
+//Local require
+var setupParse = require('./parseUtilities').setupParse;
 var LogInContainer = require('./components/login.jsx').LogInContainer;
 var CreateUserContainer = require('./components/createuser.jsx').CreateUserContainer;
 
 var AppRouter = Backbone.Router.extend({
     routes: {
-      '': 'login'
+      '': 'login',
+      'newUser/': 'newUser'
+    },
+    initialize: function(){
+      setupParse('overwatch', 'harambe');
+      $.ajaxSetup({
+        beforeSend: function(xhr){
+          console.log('beforeSend mash');
+          xhr.setRequestHeader("X-Mashape-Key", "64ygDWoKx7mshue5RJgLaVom1n5lp12Bzfejsnkr3S0j0ATb5P");
+          xhr.setRequestHeader("Accept", "application/json");
+
+        }
+      });
+
+      // console.log($.get('https://igdbcom-internet-game-database-v1.p.mashape.com/games/'));
+
+    $.get('https://igdbcom-internet-game-database-v1.p.mashape.com/games/?fields=name%2Ccover').then(function(data){
+      console.log(data);
+    });
+
     },
     login: function(){
       ReactDOM.render(
         React.createElement(LogInContainer),
+        document.getElementById('app')
+      );
+    },
+    newUser: function(){
+      ReactDOM.render(
+        React.createElement(CreateUserContainer),
         document.getElementById('app')
       );
     }
