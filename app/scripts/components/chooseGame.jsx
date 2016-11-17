324494939bde6cde086e599119d97ce59cc14f75
Colin McKeon <14mckeon@gmail.com>
2016-11-17 14:15:26 -0500

    dummy data displays

--- app/scripts/components/chooseGame.jsx
+++ app/scripts/components/chooseGame.jsx
@@ -3,13 +3,41 @@ var Backbone = require('backbone');
 
 
 var Template = require('./templates.jsx').Template;
+var gameCovers = require('../dummyData.js').gameCovers;
 
+
+var Game = React.createClass({
+  render: function (){
+
+    var gameHtml = this.props.gameList.map(function(item, index){
+      return (
+        <div className="gameCover-holder" key={item.id + index}>
+          <li><h6>{item.name}</h6></li>
+
+        </div>
+      );
+    });
+      return(
+        <div>
+          <ul>
+            {gameHtml}
+          </ul>
+        </div>
+      )
+  }
+});
 
 var ChooseGameContainer = React.createClass({
+  getInitialState: function(){
+    return {
+      game: gameCovers
+    }
+  },
   render: function(){
     return (
       <div>
         <Template />
+        <Game gameList={this.state.game} />
       </div>
     );
   }
--- app/scripts/dummyData.js
+++ app/scripts/dummyData.js
@@ -1,45 +1,29 @@
-var menuItems = [{
+var gameCovers = [{
     id: '01',
-    title: 'Half Moon Tons (6 pcs.)',
-    description: 'Crab meat and cream cheese, wrapped with wonton, skin deep fried to a nice brown, and served with our sweet and sour sauce.',
-    price: 7.95
+    name: 'Overwatch'
   }, {
     id: '02',
-    title: 'Yellow Curry',
-    description: 'Mild yellow curry, containing rich flavors of curry powder, coconut milk, potatoes, onions and carrots.',
-    price: 10.50
+    name: 'Rocket League'
   }, {
     id: '03',
-    title: 'Pad Thai',
-    description: 'One of the most famous Thai rice noodles dish cooked with egg, bean spouts, tofu and sprinkled with ground peanut.',
-    price: 10.50
+    name: 'World of Warcraft: Legion'
   }, {
     id: '04',
-    title: 'Orange Chicken',
-    description: 'Deep fried chicken sauteed in a tangy orange sauce.',
-    price: 10.50
+    name: 'Battlefield 1'
   }, {
     id: '05',
-    title: 'Cashew Nut',
-    description: 'Stir fried with cashew nuts, onions, carrots and dried crispy chilies and Thai angel sauce.',
-    price: 9.50
+    name: 'Destiny: Rise of Iron'
   }, {
     id: '06',
-    title: 'Pepper and Garlic',
-    description: 'Stir fried with freshly ground garlic, and black pepper, on top of lettuce.',
-    price: 10.00
+    name: 'Hearthstone: Heroes of Warcraft'
   }, {
     id: '07',
-    title: 'Spicy Eggplant',
-    description: 'Stir fried eggplant in black bean sauce, chilies and basil leaves.',
-    price: 11.50
+    name: 'H1Z1: King of the Kill'
   }, {
     id: '08',
-    title: 'Teriyaki',
-    description: 'Grilled chicken or beef, with vegetables on top of teriyaki sauce and special house sauce.',
-    price: 10.50
+    name: 'Counter-Strike: Global Offensive'
 }];
 
 module.exports = {
-  menuItems: menuItems
+  gameCovers: gameCovers
 };
