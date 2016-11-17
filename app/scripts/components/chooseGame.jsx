9b5ce7b1073b8dc8bfc4b572301d416d706ce1c9
Colin McKeon <14mckeon@gmail.com>
2016-11-17 16:37:00 -0500

    modal is working

--- app/scripts/components/chooseGame.jsx
+++ app/scripts/components/chooseGame.jsx
@@ -1,5 +1,7 @@
 var React = require('react');
 var Backbone = require('backbone');
+var Modal = require('react-modal');
+
 
 
 var Template = require('./templates.jsx').Template;
@@ -7,21 +9,33 @@ var gameCovers = require('../dummyData.js').gameCovers;
 
 
 var Game = React.createClass({
-  render: function (){
-
+  getInitialState: function(){
+   return { modalIsOpen: false };
+  },
+ openModal: function(){
+   this.setState({modalIsOpen: true});
+  },
+ closeModal: function(){
+    this.setState({modalIsOpen: false});
+  },
+  render: function(){
+    var self = this;
     var gameHtml = this.props.gameList.map(function(item, index){
       return (
-        <div className="col-md-3" key={item.id + index}>
-          <a href="#createSquad/">
-            <div className="gameCover-holder">
-              <h6>{item.name}</h6>
-            </div>
-          </a>
-        </div>
+
+          <div className="col-md-3" key={item.id + index}>
+              <div onClick={self.openModal} className="gameCover-holder">
+                <h6>{item.name}</h6>
+              </div>
+          </div>
+
       );
     });
       return(
         <div>
+          <Modal isOpen={this.state.modalIsOpen}>
+            <button onClick={this.closeModal}>close</button>
+          </Modal>
             {gameHtml}
         </div>
       )
