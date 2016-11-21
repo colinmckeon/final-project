e185861e9900458e06c9d083e00493f81452a53a
Colin McKeon <14mckeon@gmail.com>
2016-11-21 16:37:42 -0500

    join squad displays to screen

--- app/scripts/components/findSquad.jsx
+++ app/scripts/components/findSquad.jsx
@@ -2,15 +2,30 @@ var React = require('react');
 var Backbone = require('backbone');
 
 var Template = require('./templates.jsx').Template;
+var CreateSquadCollection = require('../models/squads.js').CreateSquadCollection;
 
 
 var FindSquad = React.createClass({
   render: function(){
+    var squadList = this.props.squadListing.map(function(squad){
+      console.log(squad);
+      return (
+        <div key={squad.objectId}>
+          <h1>{squad.players}</h1>
+          <p>{squad.message}</p>
+
+          <div>
+          <button className="btn joinSquadButton" type="submit">Join Squad <i className="fa fa-gamepad" aria-hidden="true"></i></button>
+          </div>
+
+        </div>
+      )
+    });
     return (
       <div>
         <h1 id="findSquadTitle">JOIN A SQUAD</h1>
         <div className="col-md-6 well">
-
+          {squadList}
         </div>
       </div>
     );
@@ -19,7 +34,21 @@ var FindSquad = React.createClass({
 
 
 var FindSquadContainer = React.createClass({
-  get
+  getInitialState: function(){
+    return{
+      collection: new CreateSquadCollection(),
+      squadListing: []
+    }
+  },
+  componentWillMount: function(){
+    var self = this;
+    var collection = this.state.collection;
+
+    collection.fetch().then(function(response){
+
+      self.setState({squadListing: response.results});
+    });
+  },
   render: function(){
     return (
 
@@ -27,7 +56,7 @@ var FindSquadContainer = React.createClass({
           <div className="container">
             <div className="row">
 
-              <FindSquad />
+              <FindSquad squadListing={this.state.squadListing}/>
             </div>
 
           </div>
