4a15a0240637ebb10be2c2e664029d4acdc8c6df
Colin McKeon <14mckeon@gmail.com>
2016-11-21 13:24:18 -0500

    create squad form created

--- app/scripts/components/createSquad.jsx
+++ app/scripts/components/createSquad.jsx
@@ -11,20 +11,25 @@ var CreateSquad = React.createClass({
         <div className="createSquadHolder">
 
           <div className="createSquadTitleHolder">
-            <h1 id="createSquadTitle">CREATE A SQUAD</h1>
+            <h1 id="createSquadTitle">CREATE A SQUAD <i className="fa fa-plus-circle" aria-hidden="true"></i></h1>
           </div>
+          <br />
+          <br />
 
           <form>
             <div>
               <label forHTML="playersNeeded">Players Needed for Squad: &nbsp; </label>
               <input id="playersNeeded" type="number" min="1" max="40" placeholder="minimum of 1"></input>
             </div>
+            <br />
 
             <div>
-              <label forHTML="creatorMessage">Message</label>
-              <textarea id="creatorMessage"></textarea>
+              <label forHTML="creatorMessage">Your Message to the Squad:</label>
+              <textarea id="creatorMessage" placeholder="E.g. I am looking to find a squad of competitive players who's skill level is 'blank'. Everyone needs mic, and no arguing is allowed.  Looking for players attempting to accomplish 'blank'.  Trying to start a queue in at most, 15 minutes. Let's pwn some noobs!!!!"></textarea>
             </div>
+            <br />
 
+            <button id="createNewSquadButton" className="btn" type="submit">Create Squad <i className="fa fa-gamepad" aria-hidden="true"></i></button>
           </form>
 
         </div>
