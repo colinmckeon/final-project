0643b5e539b1c4f5f576a6c75d7c2eadd516a8ba
Colin McKeon <14mckeon@gmail.com>
2016-11-29 17:34:13 -0500

    added fonts

--- app/scripts/components/userProfile.jsx
+++ app/scripts/components/userProfile.jsx
@@ -10,31 +10,38 @@ var UserProfile = React.createClass({
   render: function(){
     var gameName = this.props.gameCollection.map(function(game){
       return(
-        <li key={game.cid}>{game.get('name')}</li>
+        <li id="listedGame" key={game.cid}>{game.get('name')}</li>
       )
     });
     return (
       <div>
 
-        <div className="col-md-3 col-md-offset-1">
-          <div>
+        <div className="row">
+          <div className="col-md-3 col-md-offset-1">
             <div>
-               <img src={this.props.user.get('avatar')} id="avatarUserProfile"></img>
+              <div>
+                 <img src={this.props.user.get('avatar')} id="avatarUserProfile"></img>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-8">
+            <div className="qusernameGamertagHolder">
+              <h1 id="profileQusername">{this.props.user.get('qusername')}</h1>
+              <h5 id="gamertagLabel">XBOX Live Gamertag: </h5> &nbsp;
+              <h5 id="profileGamertag">{this.props.user.get('gamertag')}</h5>
             </div>
           </div>
         </div>
 
-        <div className="col-md-8">
-          <div className="qusernameGamertagHolder">
-            <h1 id="profileQusername">{this.props.user.get('qusername')}</h1>
-            <h5 id="gamertagLabel">XBOX Live Gamertag: </h5> &nbsp;
-            <h5 id="profileGamertag">{this.props.user.get('gamertag')}</h5>
-          </div>
-          <div>
-            <h5 id="mostPlayedGamesLabel">Most Played Games:</h5>
-            <ul>
-              {gameName}
-            </ul>
+        <div className="row">
+          <div className="col-md-8 col-md-offset-2">
+            <div id="gamesPlayedByUser">
+              <h5 id="mostPlayedGamesLabel">Games played by {this.props.user.get('qusername')}</h5>
+              <ul>
+                {gameName}
+              </ul>
+            </div>
           </div>
         </div>
 
@@ -77,12 +84,12 @@ var UserProfileContainer = React.createClass({
       <div>
         <Template>
           <div className="container">
-            <div className="row">
+
               <UserProfile
                 user={this.state.user}
                 gameCollection={this.state.gameCollection}
                 />
-            </div>
+
           </div>
         </Template>
       </div>
